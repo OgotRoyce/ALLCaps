2d3b582b866812a6bff27f6b7a1162965c4dd73f
OgotRoyce <201911397@gordoncollege.edu.ph>
2023-01-19 19:17:49 +0800

    Update tms_db.sql

--- task_management_system/database/tms_db.sql
+++ task_management_system/database/tms_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2020 at 08:37 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Generation Time: Jan 19, 2023 at 12:13 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,8 +44,9 @@ CREATE TABLE `project_list` (
 --
 
 INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`) VALUES
-(1, 'Sample Project', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, metus vitae malesuada mollis, urna nisi luctus ligula, vitae volutpat massa eros eu ligula. Nunc dui metus, iaculis id dolor non, luctus tristique libero. Aenean et sagittis sem. Nulla facilisi. Mauris at placerat augue. Nullam porttitor felis turpis, ac varius eros placerat et. Nunc ut enim scelerisque, porta lacus vitae, viverra justo. Nam mollis turpis nec dolor feugiat, sed bibendum velit placerat. Etiam in hendrerit leo. Nullam mollis lorem massa, sit amet tincidunt dolor lacinia at.&lt;/span&gt;							', 0, '2020-11-03', '2021-01-20', 2, '3,4,5', '2020-12-03 09:56:56'),
-(2, 'Sample Project 102', 'Sample Only', 0, '2020-12-02', '2020-12-31', 2, '3', '2020-12-03 13:51:54');
+(4, 'GC Dev2Dev', '												&lt;span style=&quot;color: rgba(0, 0, 0, 0.87); font-family: Poppins, sans-serif; font-size: 15.008px; text-align: justify; white-space: pre-line;&quot;&gt;GC Dev 2 Dev is a web application&amp;nbsp;&lt;/span&gt;&lt;font color=&quot;rgba(0, 0, 0, 0.870588235294118)&quot; face=&quot;Poppins, sans-serif&quot;&gt;&lt;span style=&quot;font-size: 15.008px; white-space: pre-line;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.&lt;/span&gt;&lt;/font&gt;', 0, '2022-11-26', '2022-12-06', 8, '', '2022-12-06 00:23:12'),
+(5, 'GC Go Library', 'GC Go Library is&amp;nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 0, '2022-11-03', '2022-12-12', 7, '', '2022-12-10 03:24:47'),
+(6, 'AllCaps', '&lt;p style=&quot;font-family: system-ui, -apple-system, &amp;quot;Segoe UI&amp;quot;, Roboto, &amp;quot;Helvetica Neue&amp;quot;, Arial, &amp;quot;Noto Sans&amp;quot;, &amp;quot;Liberation Sans&amp;quot;, sans-serif, &amp;quot;Apple Color Emoji&amp;quot;, &amp;quot;Segoe UI Emoji&amp;quot;, &amp;quot;Segoe UI Symbol&amp;quot;, &amp;quot;Noto Color Emoji&amp;quot;;&quot;&gt;Lorem ipsum dolor sit amet. Ad odio quam et neque illo qui recusandae consequatur id incidunt velit aut voluptas iure et unde magnam? A nisi recusandae est voluptatem incidunt sed natus dolorem in laborum quos est galisum sunt qui quia consequatur.&lt;/p&gt;&lt;p style=&quot;font-family: system-ui, -apple-system, &amp;quot;Segoe UI&amp;quot;, Roboto, &amp;quot;Helvetica Neue&amp;quot;, Arial, &amp;quot;Noto Sans&amp;quot;, &amp;quot;Liberation Sans&amp;quot;, sans-serif, &amp;quot;Apple Color Emoji&amp;quot;, &amp;quot;Segoe UI Emoji&amp;quot;, &amp;quot;Segoe UI Symbol&amp;quot;, &amp;quot;Noto Color Emoji&amp;quot;;&quot;&gt;A earum eligendi aut nobis quidem et laboriosam quia est veritatis placeat. Sit natus omnis qui autem rerum ut sint repellat aut accusamus atque quo temporibus delectus! Qui saepe sint sit culpa atque non magni numquam ut ipsum temporibus sed ipsam cumque aut accusantium quibusdam.&lt;/p&gt;											', 3, '2022-11-20', '2023-05-13', 6, '', '2023-01-18 15:44:45');
 
 -- --------------------------------------------------------
 
@@ -67,7 +68,7 @@ CREATE TABLE `system_settings` (
 --
 
 INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
-(1, 'Task Management System', 'info@sample.comm', '+6948 8542 623', '2102  Caldwell Road, Rochester, New York, 14608', '');
+(1, 'Capstone Project Team Monitoring System', 'info@sample.comm', '+6948 8542 623', 'East Tapinac, Olongapo City, Zambales', '');
 
 -- --------------------------------------------------------
 
@@ -89,10 +90,9 @@ CREATE TABLE `task_list` (
 --
 
 INSERT INTO `task_list` (`id`, `project_id`, `task`, `description`, `status`, `date_created`) VALUES
-(1, 1, 'Sample Task 1', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Fusce ullamcorper mattis semper. Nunc vel risus ipsum. Sed maximus dapibus nisl non laoreet. Pellentesque quis mauris odio. Donec fermentum facilisis odio, sit amet aliquet purus scelerisque eget.&amp;nbsp;&lt;/span&gt;													', 3, '2020-12-03 11:08:58'),
-(2, 1, 'Sample Task 2', 'Sample Task 2							', 1, '2020-12-03 13:50:15'),
-(3, 2, 'Task Test', 'Sample', 1, '2020-12-03 13:52:25'),
-(4, 2, 'test 23', 'Sample test 23', 1, '2020-12-03 13:52:40');
+(5, 4, 'Submit your Project Poster', '				Make sure to submit it on time			', 2, '2022-12-06 14:55:39'),
+(6, 4, 'Project AVP', '				I would like to see your AVP presentation of your project this night			', 3, '2022-12-06 14:58:04'),
+(7, 4, 'Review and Debug Errors', '				I saw some issues on your web app that needs to be fixed immediately			', 3, '2022-12-09 20:54:09');
 
 -- --------------------------------------------------------
 
@@ -116,11 +116,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
-(1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, 'no-image-available.png', '2020-11-26 10:57:04'),
-(2, 'John', 'Smith', 'jsmith@sample.com', '1254737c076cf867dc53d60a0364f38e', 2, '1606978560_avatar.jpg', '2020-12-03 09:26:03'),
-(3, 'Claire', 'Blake', 'cblake@sample.com', '4744ddea876b11dcb1d169fadf494418', 3, '1606958760_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 09:26:42'),
-(4, 'George', 'Wilson', 'gwilson@sample.com', 'd40242fb23c45206fadee4e2418f274f', 3, '1606963560_avatar.jpg', '2020-12-03 10:46:41'),
-(5, 'Mike', 'Williams', 'mwilliams@sample.com', '3cc93e9a6741d8b40460457139cf8ced', 3, '1606963620_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 10:47:06');
+(1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, 'no-image-available.png', '2022-11-26 10:57:04'),
+(6, 'Royce', 'Ogot', '201911397@gordoncollege.edu.ph', '58b9c86a0d0a5118f5b74b1835f72cc8', 2, '1670246580_BSIT_OGOT_ROYCE.jpeg', '2022-12-05 21:23:27'),
+(7, 'Lloyd', 'Mariano', 'lloydmariano@gmail.com', '4bf183ea9cf95d311518e7ed542a9f89', 2, '1670309280_1606978560_avatar.jpg', '2022-12-06 14:48:24'),
+(8, 'Ken Matthew', 'Ammay', 'kenammay@gmail.com', 'b3906075d9ee220bf34c854b453a238b', 2, 'no-image-available.png', '2022-12-10 03:22:27'),
+(9, 'Von', 'Tandoc', 'vontandoc@gmail.com', 'c292c58dc89573a27c7623cf875c5686', 2, 'no-image-available.png', '2022-12-10 03:34:09');
 
 -- --------------------------------------------------------
 
@@ -147,10 +147,10 @@ CREATE TABLE `user_productivity` (
 --
 
 INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `comment`, `subject`, `date`, `start_time`, `end_time`, `user_id`, `time_rendered`, `date_created`) VALUES
-(1, 1, 1, '							&lt;p&gt;Sample Progress&lt;/p&gt;&lt;ul&gt;&lt;li&gt;Test 1&lt;/li&gt;&lt;li&gt;Test 2&lt;/li&gt;&lt;li&gt;Test 3&lt;/li&gt;&lt;/ul&gt;																			', 'Sample Progress', '2020-12-03', '08:00:00', '10:00:00', 1, 2, '2020-12-03 12:13:28'),
-(2, 1, 1, '							Sample Progress						', 'Sample Progress 2', '2020-12-03', '13:00:00', '14:00:00', 1, 1, '2020-12-03 13:48:28'),
-(3, 1, 2, '							Sample						', 'Test', '2020-12-03', '08:00:00', '09:00:00', 5, 1, '2020-12-03 13:57:22'),
-(4, 1, 2, 'asdasdasd', 'Sample Progress', '2020-12-02', '08:00:00', '10:00:00', 2, 2, '2020-12-03 14:36:30');
+(5, 4, 0, '&lt;span style=&quot;color: rgba(0, 0, 0, 0.87); font-family: Poppins, sans-serif; font-size: 15.008px; text-align: justify; white-space: pre-line;&quot;&gt;Uploaded my team&rsquo;s 2nd RESEARCH PRESENTATION Video presentation on&amp;nbsp; the Google Drive.&lt;/span&gt;', '2nd RESEARCH PRESENTATION Video presentation', '2022-12-05', '10:05:00', '10:25:00', 6, 0.333333, '2022-12-06 00:26:16'),
+(6, 4, 0, 'Edited my&lt;span style=&quot;color: rgba(0, 0, 0, 0.87); font-family: Poppins, sans-serif; font-size: 15.008px; text-align: justify; white-space: pre-line;&quot;&gt;&amp;nbsp;2nd RESEARCH PRESENTATION Video presentation&lt;/span&gt;', '2nd RESEARCH PRESENTATION Video presentation', '2022-12-05', '00:00:00', '23:30:00', 6, 23.5, '2022-12-06 00:27:41'),
+(7, 4, 6, '													', '2nd RESEARCH PRESENTATION Video presentation', '2022-12-04', '19:00:00', '03:30:00', 6, -15.5, '2022-12-06 14:59:27'),
+(8, 4, 7, '&lt;p style=&quot;font-family: system-ui, -apple-system, &amp;quot;Segoe UI&amp;quot;, Roboto, &amp;quot;Helvetica Neue&amp;quot;, Arial, &amp;quot;Noto Sans&amp;quot;, &amp;quot;Liberation Sans&amp;quot;, sans-serif, &amp;quot;Apple Color Emoji&amp;quot;, &amp;quot;Segoe UI Emoji&amp;quot;, &amp;quot;Segoe UI Symbol&amp;quot;, &amp;quot;Noto Color Emoji&amp;quot;;&quot;&gt;Lorem ipsum dolor sit amet. Ad odio quam et neque illo qui recusandae consequatur id incidunt velit aut voluptas iure et unde magnam? A nisi recusandae est voluptatem incidunt sed natus dolorem in laborum quos est galisum sunt qui quia consequatur.&lt;/p&gt;&lt;p style=&quot;font-family: system-ui, -apple-system, &amp;quot;Segoe UI&amp;quot;, Roboto, &amp;quot;Helvetica Neue&amp;quot;, Arial, &amp;quot;Noto Sans&amp;quot;, &amp;quot;Liberation Sans&amp;quot;, sans-serif, &amp;quot;Apple Color Emoji&amp;quot;, &amp;quot;Segoe UI Emoji&amp;quot;, &amp;quot;Segoe UI Symbol&amp;quot;, &amp;quot;Noto Color Emoji&amp;quot;;&quot;&gt;A earum eligendi aut nobis quidem et laboriosam quia est veritatis placeat. Sit natus omnis qui autem rerum ut sint repellat aut accusamus atque quo temporibus delectus! Qui saepe sint sit culpa atque non magni numquam ut ipsum temporibus sed ipsam cumque aut accusantium quibusdam.&lt;/p&gt;													', 'Errors debugged', '2023-01-17', '07:49:00', '19:49:00', 1, 12, '2023-01-18 15:49:37');
 
 --
 -- Indexes for dumped tables
@@ -194,7 +194,7 @@ ALTER TABLE `user_productivity`
 -- AUTO_INCREMENT for table `project_list`
 --
 ALTER TABLE `project_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `system_settings`
@@ -206,19 +206,19 @@ ALTER TABLE `system_settings`
 -- AUTO_INCREMENT for table `task_list`
 --
 ALTER TABLE `task_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user_productivity`
 --
 ALTER TABLE `user_productivity`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
